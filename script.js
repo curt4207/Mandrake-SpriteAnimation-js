4e0d2496288dd9fdbb8a695f9a053a513b261f93
Curtis Clayton <curtisclayton4270@gmail.com>
2023-06-15 01:06:43 -0400

    Added sprite animation

--- script.js
+++ script.js
@@ -7,13 +7,49 @@ window.addEventListener("load", function() {
     console.log( "ctx", ctx);
 
     class Mandrake {
-        constructor() {
+        constructor(canvasWidth, canvasHeight) {
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
             this.image =document.getElementById("mandrake");
             this.spriteWidth = 256;
             this.spriteHeight = 256;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
-            this.x = 0;
-        }
+            this.scale = 0.7;
+            this.x = this.canvasWidth/2 - this.width * this.scale/2;
+            this.y = this.canvasHeight/2 - this.height * this.scale/2;
+            this.minFrame = 0;
+            this.maxFrame = 355;
+            this.frame = 0;
+
+            this.frameX = 3;
+            this.frameY = 2;
+        };
+
+        draw(context) {
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
+                 this.spriteWidth, this.spriteHeight, this.x, this.y, this.width * this.scale, this.height * this.scale);
+        };
+
+        update() {
+            // if(this.frameX < 17) this.frameX ++;
+            // else this.frameX = 0;
+            if(this.frame < this.maxFrame) this.frame ++;
+            else this.frame =this.minFrame;
+            this.frameX =this.frame % 18;
+            this.frameY = Math.floor(this.frame / 18);
+        };
     }
-})
+
+    const mandrake = new Mandrake(canvas.width, canvas.height);
+    // console.log( "mandrake", mandrake);
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        mandrake.draw(ctx);
+        mandrake.update();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
